be0941e2b3e643bfc5b99b5faf9461d1823ee134
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-25 13:03:48 +0200

    Aggiungere i file di progetto.

---
+++ net-calculator-tester/Classes/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester.Classes
+{
+    internal class Calculator
+    {
+        public static T NumberSumm<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 + number2;
+        }
+        public static T NumberDiff<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 - number2;
+        }
+        public static T NumberMultiplier<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 * number2;
+        }
+        public static T NumberDivision<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 / number2;
+        }
+    }
+}
